6ff89e8a0b09b2542bf8e42bccdd2003680cdc25
Stephen Yeargin <stephenyeargin@users.noreply.github.com>
2014-09-05 22:19:16 -0500

    Update random.php

--- random.php
+++ random.php
@@ -2,7 +2,7 @@
 
 require_once('config.php');
 
-$ch = curl_init('http://api.brewerydb.com/v2//beer/random?key=' . BREWERY_DB_API_KEY);
+$ch = curl_init('http://api.brewerydb.com/v2/beer/random?key=' . BREWERY_DB_API_KEY);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $return = curl_exec($ch);
 
